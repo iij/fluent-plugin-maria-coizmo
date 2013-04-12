a21fb034f64b105e9779cd5ea0bf219b45233a44
hiro-su <h.sugipon@gmail.com>
2013-04-12 20:54:49 +0900

    bugfix

--- lib/fluent/plugin/out_maria_coizmo.rb
+++ lib/fluent/plugin/out_maria_coizmo.rb
@@ -11,7 +11,17 @@ module Fluent
     
     def initialize
       super
-      @sensor_id_cache = {}
+      @sensor_id_cache = Hash.new
+    end
+
+    def configure(conf)
+      super
+      @init = {
+        :host => @host,
+        :username => @username,
+        :password => @password,
+        :database => @database
+      }
     end
 
     def format(tag, time, record)
@@ -19,20 +29,14 @@ module Fluent
     end
 
     def write(chunk)
-      init = {
-        :host => @host,
-        :username => @username,
-        :password => @password,
-        :database => @database
-      }
-
-      handler = Mysql2::Client.new(init)
+      handler = Mysql2::Client.new(@init)
 
       chunk.msgpack_each do |tag,time,data|
-        sensor_id = @sensor_id_cache["#{data["host"]}.#{data["device"]}"]
+        sensor_id = @sensor_id_cache[data["device"]]
         if sensor_id.nil?
           sensor_id = get_sensor_id(handler,data["host"],data["device"],data["unit"])
-          @sensor_id_cache["#{data["host"]}.#{data["device"]}"] = sensor_id
+          $log.info "cached sensor_id: #{sensor_id}"
+          @sensor_id_cache[data["device"]] = sensor_id
         end
 
         sql = "INSERT INTO data (sensor_id,time,value,leverage) VALUES (#{sensor_id},from_unixtime(#{time}),#{data["value"]},#{data["leverage"]})"
@@ -40,47 +44,40 @@ module Fluent
       end
 
       handler.close
-    rescue => ex
-      $log.error ex
-      exit
     end
-
-    private
 
     def get_sensor_id(handler,host_name,device,unit)
       sql = "SELECT sensor_id FROM sensor JOIN host ON sensor.host_id=host.host_id WHERE host.host_name='#{host_name}' AND device='#{device}'" 
+      $log.info sql
       result = handler.query(sql).each do |sensor_id|
         sensor_id.each do |key, val|
-          sleep 0.1
           return val
         end
       end
       if result.empty?
         host_id = get_host_id(handler,host_name)
         set_sensor(handler, device, host_id, unit)
-        get_sensor_id(handler, host_name)
+        get_sensor_id(handler, host_name, device, unit)
       end
-     rescue => ex
-       raise ex
     end
 
     def get_host_id(handler,host_name)
       sql = "SELECT host_id FROM host WHERE host_name='#{host_name}'"
+      $log.info sql
+      sleep 0.5
       handler.query(sql).each do |host_id|
         host_id.each do |key, val|
-          sleep 0.1
           return val
         end
       end
-    rescue => ex
-      raise ex
     end
 
     def set_sensor(handler,device,host_id,unit)
       sql = "INSERT INTO sensor (device, unit, host_id) VALUES ('#{device}', '#{unit}', #{host_id})"
+      $log.info sql
+      sleep 0.5
       handler.query(sql)
-    rescue => ex
-      raise ex
     end
+
   end
 end
